ad71859d807b05c6eeba61e82fb1341601544ec4
leo-de-los-rios <leodelosriosprogramacion@gmail.com>
2024-07-30 19:19:21 -0300

    Update: Unidad 1

--- Unidad 1 - Programación Funcional/ejercicio_5.hs
+++ Unidad 1 - Programación Funcional/ejercicio_5.hs
@@ -6,11 +6,19 @@ pares :: [Integer] -> Int
 pares lista = length [x | x <- lista, even x]
 
 -- Método 2: Función Recursiva
-paresRecursiva :: [Integer] -> Int
-paresRecursiva [] = 0
-paresRecursiva (x:xs)
-    | even x = 1 + paresRecursiva xs
-    | otherwise = paresRecursiva xs
+paresRecursivaGuardas :: [Integer] -> Int
+paresRecursivaGuardas [] = 0
+paresRecursivaGuardas (x:xs)
+    | even x = 1 + paresRecursivaGuardas xs
+    | otherwise = paresRecursivaGuardas xs
+
+-- Método 2: Función Recursiva
+paresRecursivaAlternativa :: [Integer] -> Int
+paresRecursivaAlternativa [] = 0
+paresRecursivaAlternativa (x:xs) =
+    if even x
+    then 1 + paresRecursivaAlternativa xs
+    else paresRecursivaAlternativa xs
 
 -- Método 3: Forma Funcional
 paresFuncional :: [Integer] -> Int
@@ -19,3 +27,13 @@ paresFuncional lista = length (filter even lista)
 -- Método 4: Función Lambda
 paresLambda :: [Integer] -> Int
 paresLambda lista = length (filter (\x -> even x) lista)
+
+-- Función main para pruebas
+main :: IO ()
+main = do
+    let lista = [1, 2, 3, 4, 5, 6]
+    putStrLn $ "Cantidad de elementos pares (Método Básico): " ++ show (pares lista)
+    putStrLn $ "Cantidad de elementos pares (Recursiva con Guardas): " ++ show (paresRecursivaGuardas lista)
+    putStrLn $ "Cantidad de elementos pares (Recursiva con Alternativa): " ++ show (paresRecursivaAlternativa lista)
+    putStrLn $ "Cantidad de elementos pares (Funcional): " ++ show (paresFuncional lista)
+    putStrLn $ "Cantidad de elementos pares (Lambda): " ++ show (paresLambda lista)
